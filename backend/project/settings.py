7ff0309b337e2189eaaa66677b8546312f9b02db
aliharby12 <aliharby565@gmail.com>
2024-12-23 09:44:22 +0200

    Integrate .env file using python-decouple for secure and manageable environment variables

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,13 +24,13 @@ CORS_ALLOW_ALL_ORIGINS = True
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$4yr-t+q3owtg##0vk*!!ra&bh2nflr!1noot-x4a%evou$n*f"
+SECRET_KEY = config("SECRET_KEY", default="django-insecure-default-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = ["*"]
-
+# Allowed hosts
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1").split(",")
 
 # Application definition
 
